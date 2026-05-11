b24fa792e7c34c62d7a1dad2228a9d70916da4fd
Fabricio Rostand Morais <fabriciorostand2000@gmail.com>
2026-05-10 22:36:48 -0300

    init: Spring Boot and Maven project

---
+++ src/test/java/br/com/fiap/esg/EsgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.esg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
