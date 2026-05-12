84e60ea6779877cf2e841884ab7b7ec1426c137d
Cleyssonfreitas <cleyssonfreitas30@gmail.com>
2026-05-11 21:55:38 -0300

    Criado a entidade Dispositivo.java

---
+++ src/main/java/br/com/fiap/esg/model/Dispositivo.java
@@ -0,0 +1,37 @@
+package br.com.fiap.esg.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+
+@Data
+@Entity
+@Table (name = "ESG_DISPOSITIVO")
+public class Dispositivo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_DISPOSITIVO")
+    @SequenceGenerator(name = "SEQ_DISPOSITIVO", sequenceName = "SEQ_DISPOSITIVO", allocationSize = 1)
+    @Column(name = "id_dispositivo")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_unidade_consumidora", nullable = false)
+    private  UnidadeConsumidora unidadeConsumidora;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "potencia_nominal", nullable = false)
+    private double potencialNominal;
+
+    @Column(name = "status", nullable = false, length = 20)
+    private String status;
+
+    @Column(name = "consumo_minimo_ativo", nullable = false)
+    private Double consumoMinimoAtivo;
+
+    @Column(name = "tempo_ociosidade_limite", nullable = false)
+    private  Integer tempoOsiosidadeLimite;
+}
